968eb5ab2d45b6b5a63bdf2b65cb83d34295f696
Josh Hammer <jph33@outlook.com>
2025-11-18 08:32:11 -0500

    ruff formatting + nix shell for python package

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "shell";
+  inputs.unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  outputs = inputs: let
+    system = "x86_64-linux";
+    pkgs = import inputs.unstable {inherit system;};
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          (python3.withPackages (p: with p; [tatsu]))
+        ];
+      };
+    };
+  };
+}
